8eea3ff0d1ab236e37782a9c695834d96adeff56
eximius8 <mikhail.trunov@gmail.com>
2018-12-06 07:25:17 +0100

    Create analysis.py

---
+++ analysis.py
@@ -0,0 +1,18 @@
+import csv
+experiments={}
+with open('data.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        for Eid in [445,446,447,448,449,450,458]:
+            try:
+                if (Eid, int(row["S"+ str(Eid)])) in experiments:                    
+                    experiments[(Eid, int(row["S"+ str(Eid)]))].append(float(row["HV"+ str(Eid)]))
+                elif row["S"+ str(Eid)]:                    
+                    experiments[(Eid, int(row["S445"]))]=[float(row["HV"+ str(Eid)])]
+            except:
+                pass
+                    
+
+        
+
+print(experiments)
