03a96f5f0030b5772c9a64e2596cf3ab1116b26d
eximius8 <mikhail.trunov@gmail.com>
2018-12-12 21:21:57 +0100

    Update csv_to_txt.sh

--- csv_to_txt.sh
+++ csv_to_txt.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-  2 touch 'id.txt'
-  3 fil ='id.txt'
-  4 echo "" > id.txt 
-  5 for f in *csv
-  6 do
-  7    echo -n $f >> id.txt
-  8    tail -2 $f | head -1 >> id.txt
-  9  done
- 10  less id.txt 
+touch 'id.txt'
+echo "" > id.txt 
+for f in *csv
+ do
+    echo -n $f >> id.txt
+    tail -2 $f | head -1 >> id.txt
+ done
+less id.txt 
