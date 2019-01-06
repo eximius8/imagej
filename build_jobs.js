b475479fc39d27eeab86873a4e105c037b589ba7
eximius8 <mikhail.trunov@gmail.com>
2019-01-06 01:36:56 +0100

    Added for loop

--- build_jobs.js
+++ build_jobs.js
@@ -3,12 +3,17 @@ function main() {
     var radcyl = 4;
     var hcyl = 70;
     var resolut = 30;
-    cyl1=CSG.cylinder({
-        start: [40, 20, 0],
-        end: [40, 20, hcyl],
+    var coord = [[40, 20], [40, -20], [-40, 20], [-40, -20]];
+    var corlength = coord.length;
+    for (var i = 0; i < corlength; i++)
+    {
+        var cyl=CSG.cylinder({
+        start: coord[i].concat([0]),
+        end: coord[i].concat([hcyl]),
         radius: radcyl,
         resolution: resolut
-    });
-    w.push(cyl1);
+        });
+         w.push(cyl);
+    }
     return w;
 }
