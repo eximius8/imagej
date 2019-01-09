fb5a470ae580e079199f097a0dc66ed15511877b
eximius8 <mikhail.trunov@gmail.com>
2019-01-09 09:02:19 +0100

    Added Fatique

--- build_jobs.js
+++ build_jobs.js
@@ -1,15 +1,15 @@
 var w = new Array();
 function main() {
-    // Vertical Tensile cylinders
+// Vertical Tensile cylinders
     var radcyl = 4;
     var hcyl = 70;
     var resolut = 5;
     var cyl1coord = [
-        [15, 15],
-        [15, -15],
-        [-15, 15],
-        [-15, -15],
-        [0,-28]];
+        [-90, 10],
+        [-90, -10],
+        [-90, -30],
+        [-90, -50],
+        [-90,-70]];
 // Horizontal Tensile cylinders
     var cyl2uber = 6;
     var angle=1.05*Math.PI;
@@ -48,14 +48,49 @@ function main() {
         [80,-20,0],
         [80,-40,0]
         ];
+// Charpy specimens flat
+    var dist_from_surface=cyl2uber;
+    var charpcoord2 =[
+        [-40,-80,dist_from_surface],
+        [40,-80,dist_from_surface],
+        [20,-80,dist_from_surface],
+        [0,-80,dist_from_surface],
+        [-20,-80,dist_from_surface]
+        ];
 // cubes 8x8x13
     var cubecoord =[
         [60,-60,0],
         [60,-80,0],
         [60,0,0],
         [60,-20,0],
-        [60,-40,0]
+        [60,-40,0],
+        [-70,-60,0],
+        [-70,-80,0],
+        [-70,0,0],
+        [-70,-20,0],
+        [-70,-40,0]
         ];
+// Fatique samples
+    var fatrad=6;
+    var fath=84;
+    var fatique1 =[
+        [-95,80],
+        [-75,90],
+        [-55,90],
+        [-35,90],
+        [-15,90]
+        ];
+// Fatique stand
+    for (var i = 0; i < fatique1.length; i++)
+    {
+        var fatcyl1=CSG.cylinder({
+        start: fatique1[i].concat([0]),
+        end: fatique1[i].concat([fath]),
+        radius: fatrad,
+        resolution: resolut
+        });
+         w.push(fatcyl1);
+    }
     for (var i = 0; i < cyl1coord.length; i++)
     {
         var cyl1=CSG.cylinder({
@@ -84,6 +119,7 @@ function main() {
     {
         var lecocyl=CSG.cylinder({
         start: lecocoord[k].concat([0]),
+
         end: lecocoord[k].concat([lecoh]),
         radius: lecorad,
         resolution: resolut
@@ -100,6 +136,18 @@ function main() {
             ]
         });
          w.push(charpcube);
+    }
+    // Charpy horizontal
+    for (var l = 0; l < charpcoord2.length; l++)
+    {
+        var charpcube2=CSG.cube({ 
+            corner1: charpcoord2[l],
+            corner2: [charpcoord2[l][0]+10,
+            charpcoord2[l][1]+55,
+            charpcoord2[l][2]+10 // Standard 55
+            ]
+        });
+         w.push(charpcube2);
     }
      for (var n = 0; n < cubecoord.length; n++)
     {
